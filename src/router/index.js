176d8aecae3fa163186858e45333397b1397f3eb
inha915 <dlsgk0915@gmail.com>
2025-07-17 12:42:41 +0900

    커뮤니티 라우팅 처리, script 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import CommunityList from '@/components/community/CommunityList.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +10,12 @@ const router = createRouter({
       name: 'home',
       component: Home,
     },
-   
+    {
+      path: '/community',
+      name: 'community',
+      component: CommunityList,
+    },
   ],
-})
+});
 
-export default router
+export default router;
