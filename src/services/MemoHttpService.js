22fd7f5914f28e092ad46a4a598630612576ded0
jinseungbok <92.bokjs@gmail.com>
2025-07-21 17:55:41 +0900

    [docs]:250721_업로드 수정2

--- src/services/MemoHttpService.js
+++ src/services/MemoHttpService.js
@@ -4,37 +4,34 @@ console.log("API_URL:", import.meta.env.VITE_API_URL);
 
 class MemoHttpService {
   async create(userId, formData) {
-    const res = await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/otd/memo/${userId}`,
-      formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      }
-    );
+    const res = await axios.post(`/api/otd/memo/${userId}`, formData, {
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+});
     return res.data;
   }
 
-  async findAll(params) {
-    const res = await axios.get("/", { params });
-    return res.data;
-  }
+async findAll(params) {
+  const res = await axios.get("/api/otd/memo", { params });
+  return res.data;
+}
 
-  async findById(id) {
-    const res = await axios.get(`/${id}`);
-    return res.data;
-  }
+async findById(id) {
+  const res = await axios.get(`/api/otd/memo/${id}`);
+  return res.data;
+}
 
-  async modify(id, jsonBody) {
-    const res = await axios.put(`/${id}`, jsonBody);
-    return res.data;
-  }
+async modify(id, jsonBody) {
+  const res = await axios.put(`/api/otd/memo`, jsonBody);
+  return res.data;
+}
 
-  async deleteById(id) {
-    const res = await axios.delete(`/${id}`);
-    return res.data;
-  }
+async deleteById(id) {
+  const res = await axios.delete(`/api/otd/memo?id=${id}`);
+  return res.data;
+}
+
 }
 
 export default new MemoHttpService();
