c55a2f325a39eedd81c9e4892c6fa50f85185420
greenhc502 <greenhc502@gmail.com>
2025-07-21 14:30:18 +0900

    오타 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,113 +1,91 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import CommunityList from "@/components/community/CommunityList.vue";
-import ReminderHome from "@/components/reminder/ReminderHome.vue";
-import RoutineHome from "@/components/routine/RoutineHome.vue";
-import ReminderForm from "@/components/reminder/ReminderForm.vue";
-import MealForm from "@/components/meal/MealForm.vue";
-import HealthMain from "@/views/health/HealthMain.vue";
-import ElogDetail from "@/views/health/ElogDetail.vue";
-import ElogForm from "@/views/health/ElogForm.vue";
-import HlogDetail from "@/views/health/HlogDetail.vue";
-import HlogForm from "@/views/health/HlogForm.vue";
-=======
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+
 import CommunityList from '@/components/community/CommunityList.vue';
 import ReminderHome from '@/components/reminder/ReminderHome.vue';
 import RoutineHome from '@/components/routine/RoutineHome.vue';
 import ReminderForm from '@/components/reminder/ReminderForm.vue';
-
-import Join from '@/views/Join.vue';
-import Login from '@/views/Login.vue';
-import Profile from '@/views/Profile.vue';
-
 import MealForm from '@/components/meal/MealForm.vue';
->>>>>>> 73850258ee489ef750b86f02d55413454bf1d1bd
+
+import HealthMain from '@/views/health/HealthMain.vue';
+import ElogDetail from '@/views/health/ElogDetail.vue';
+import ElogForm from '@/views/health/ElogForm.vue';
+import HlogDetail from '@/views/health/HlogDetail.vue';
+import HlogForm from '@/views/health/HlogForm.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home,
     },
     {
-      path: "/community",
-      name: "community",
+      path: '/community',
+      name: 'community',
       component: CommunityList,
     },
     {
-      path: "/reminder",
-      name: "reminder",
+      path: '/reminder',
+      name: 'reminder',
       component: ReminderHome,
     },
     {
-      path: "/routine",
-      name: "routine",
+      path: '/routine',
+      name: 'routine',
       component: RoutineHome,
     },
     {
-      path: "/reminderadd",
-      name: "reminderadd",
+      path: '/reminderadd',
+      name: 'reminderadd',
       component: ReminderForm,
     },
     {
-<<<<<<< HEAD
-      path: "/meal",
-      name: "MealForm",
+      path: '/meal',
+      name: 'MealForm',
       component: MealForm,
     },
     {
-      path: "/health",
-      name: "healthMain",
+      path: '/health',
+      name: 'healthMain',
       component: HealthMain,
     },
     {
-      path: "/elog/:exerciselog_id",
-      name: "ElogDetail",
+      path: '/elog/:exerciselog_id',
+      name: 'ElogDetail',
       component: ElogDetail,
     },
     {
-      path: "/elog/add",
-      name: "ElogForm",
+      path: '/elog/add',
+      name: 'ElogForm',
       component: ElogForm,
     },
     {
-      path: "/hlog/:healthlog_id",
-      name: "HlogDetail",
+      path: '/hlog/:healthlog_id',
+      name: 'HlogDetail',
       component: HlogDetail,
     },
     {
-      path: "/hlog/add",
-      name: "HlogForm",
+      path: '/hlog/add',
+      name: 'HlogForm',
       component: HlogForm,
     },
-=======
-
+    {
       path: '/signup',
       name: 'join',
       component: Join,
-    }, 
+    },
     {
       path: '/login',
       name: 'login',
       component: Login,
-    }, 
+    },
     {
       path: '/profile',
       name: 'profile',
       component: Profile,
-    }, 
-{
-      path: '/meal',
-      name: 'MealForm',
-      component: MealForm
-    }
-
->>>>>>> 73850258ee489ef750b86f02d55413454bf1d1bd
+    },
   ],
 });
 
