b0117566d16e3e32f16f4de66a1e1c773d8aa62f
iddadda <iddadda241@gmail.com>
2025-07-21 18:01:11 +0900

    vuetify 아이콘 안 보여서 라이브러리 추가 설치

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,7 @@ import * as directives from "vuetify/directives";
 
 import { VCalendar } from "vuetify/labs/VCalendar";
 // 캘린더는 vuetify lab에서 가져와쓰는거라 추가로 import했음
+import "@mdi/font/css/materialdesignicons.css";
 
 export default createVuetify({
   components: {
