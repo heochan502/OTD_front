0339d68db6c1a2f2b808385bc385a400ee047a0f
greenhc502 <greenhc502@gmail.com>
2025-07-24 00:25:39 +0900

    [기능] 음식검색

--- src/services/meal/mealService.js
+++ src/services/meal/mealService.js
@@ -4,10 +4,10 @@ axios.defaults.baseURL = 'api/OTD/';
 
 
 
-export const getFoodNames = async (foodName) => {
+export const getFoodNames = async (foodInfo) => {
     
-  console.log('json', foodName);
-    const res = await axios.get('meal',{params : {foodName}});
+  console.log('json', foodInfo);
+    const res = await axios.get('meal', {params : foodInfo});
     if (res.status !== 200) {
         throw new Error(`Error fetching food names: ${res.statusText}`);
     }
