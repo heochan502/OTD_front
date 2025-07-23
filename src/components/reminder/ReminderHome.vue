d31d73ba4f96e750d9b496b2c153e750ecb23a21
aspynlyn <lyn.aspyn@gmail.com>
2025-07-23 12:36:06 +0900

    수정의 수정

--- src/components/reminder/ReminderHome.vue
+++ src/components/reminder/ReminderHome.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, onMounted } from 'vue';
 import { getByMonth } from '@/services/reminder/reminderService';
+import { getByDay } from '@/services/reminder/reminderService';
 import Calendar from '@/components/reminder/Calendar.vue';
 
 const today = new Date();
@@ -9,7 +10,7 @@ const todayYear = today.getFullYear();
 const todayMonth = today.getMonth() + 1;
 const todayDate = today.getDate();
 
-const state = reactive({ reminderDate: [] });
+const state = reactive({ reminderDate: [], todayReminder: [] });
 
 const getDate = async (date) => {
   const res = await getByMonth(date.year, date.month);
@@ -21,6 +22,15 @@ const getDate = async (date) => {
   console.log('res1', res);
   console.log('res2', state.reminderDate);
 };
+
+onMounted(async () => {
+  const res = await getByDay(todayYear, todayMonth, todayDate);
+  if (res === undefined || res.status !== 200) {
+    alert('오류발생');
+    return;
+  }
+  state.reminderDate = res.data;
+});
 </script>
 
 <template>
@@ -36,9 +46,12 @@ const getDate = async (date) => {
         <span class="list_date"
           >{{ todayYear }}년 {{ todayMonth }}월 {{ todayDate }}일</span
         >
-      </div>
-      <div>
-        <!-- 일정리스트 -->
+        <ul v-if="state.todayReminder">
+          <li v-for="item in state.todayReminder" :key="item.id">
+            <span>{{item.title}}</span>
+          </li>
+        </ul>
+        <span v-else>오늘은 한가한 하루네요!</span>
       </div>
     </router-link>
   </div>
--- src/components/reminder/ReminderList.vue
+++ src/components/reminder/ReminderList.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 const state = reactive({
   reminder: [],
 });
+
+const dowImage = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const content = ref(false);
+
+const viewDetail = () => {
+  content.value = !content.value;
+};
 </script>
 <template>
   <div>
@@ -11,17 +18,39 @@ const state = reactive({
       <router-link to="/reminderform">일정 추가하기</router-link>
     </div>
     <ul class="remider">
-      <template v-for="item in state.reminder">
+      <template v-for="reminder in state.reminder" :key="reminder.id">
         <li>
-          <span class="title">{{ item.title }}</span>
-          <!-- 이미지 -->
-          <span class="date">{{ item.date }}</span>
-          <!-- 알람 -->
-          <img
-            v-if="state.reminder.content"
-            src="/src/image/button.png"
-            alt="상세보기"
-          />
+          <div>
+            <span class="title">{{ reminder.title }}</span>
+            <img
+              v-for="(dow, index) in dowImage"
+              :key="dow"
+              :src="`/src/image/${dow}_${
+                reminder.repeat && reminder.repeatdow.includes(index)
+                  ? 'on'
+                  : 'off'
+              }.png`"
+              :alt="dow"
+            />
+            <span class="date">{{ reminder.date }}</span>
+            <img
+              :src="
+                state.reminder.alarm
+                  ? '/src/image/alarm_on.png'
+                  : '/src/image/alarm_off.png'
+              "
+              alt="알람 유무"
+            />
+            <img
+              v-if="state.reminder.content"
+              src="/src/image/button.png"
+              alt="상세보기"
+              @click="viewDetail"
+            />
+          </div>
+          <div v-if="content">
+            {{ state.reminder.conctent }}
+          </div>
         </li>
         <img src="/src/image/delete.png" alt="삭제" />
         <router-link to="reminderform>"
