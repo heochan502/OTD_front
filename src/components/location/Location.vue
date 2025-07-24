ca7b93603987e586b6d79edb5df0e3e2562db8e8
d-kjh <dkjh9942@gmail.com>
2025-07-24 17:35:31 +0900

    list

--- src/components/location/Location.vue
+++ src/components/location/Location.vue
@@ -1,12 +1,17 @@
 <script setup>
-import { ref, reactive, watch } from 'vue';
-import { getLocalName, saveLocation } from '@/services/weather/locationService';
+import { ref, reactive, watch, onMounted } from 'vue';
+import {
+  getLocalName,
+  saveLocation,
+  getLocalList,
+} from '@/services/weather/locationService';
 
 const keyword = ref('');
 const selectedLocation = ref(null);
 const state = reactive({
   items: [], // 지역 검색 결과 리스트
   autocomplete: [], // 자동완성용 리스트
+  list: [],
 });
 
 const searchLocation = async () => {
@@ -16,7 +21,6 @@ const searchLocation = async () => {
   }
   const res = await getLocalName(keyword.value);
   state.items = res.data;
-  console.log('test', res.data);
   state.autocomplete = res.data.map(
     (item) => `${item.city} ${item.county} ${item.town}`
   );
@@ -33,18 +37,23 @@ const selectLocation = async () => {
     alert('지역 정보가 일치하지 않습니다');
     return;
   }
-  try {
-    await saveLocation(selectedLocation.value.localId);
-    alert('지역이 저장되었습니다');
-  } catch (e) {
-    console.log(e);
-    alert('저장에 실패했습니다');
-  }
+  await saveLocation(selectedLocation.value.localId);
+  alert('지역이 저장되었습니다');
 };
 
 watch(keyword, async (val) => {
   if (!val || typeof val !== 'string' || !val.trim()) return {};
   await searchLocation();
+});
+
+const LocalList = async () => {
+  const res = await getLocalList();
+  state.list = res.data;
+  console.log('locallist', state.list);
+};
+
+onMounted(() => {
+  LocalList();
 });
 </script>
 
@@ -61,8 +70,22 @@ watch(keyword, async (val) => {
       @keyup.enter="searchLocation"
     ></v-combobox>
   </div>
+  <div class="list">
+    <h2>저장한 지역 목록</h2>
+    <ul>
+      <li v-for="(item, index) in state.list" :key="index">
+        {{ item.city }} {{ item.county }} {{ item.town }}
+      </li>
+    </ul>
+  </div>
   <div class="mt-3">
     <p>선택된 지역: {{ keyword }}</p>
     <button class="btn btn-primary" @click="selectLocation">저장</button>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.list {
+  color: black;
+}
+</style>
