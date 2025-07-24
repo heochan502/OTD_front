f1d38aabb1d9c5e3ef4d0405bbcbd5b19dabb5da
d-kjh <dkjh9942@gmail.com>
2025-07-24 11:32:23 +0900

    locaionSearch

--- src/views/weather/weather.vue
+++ src/views/weather/weather.vue
@@ -35,8 +35,8 @@ const skyEmoji = computed(() => {
 const dayTimes = computed(() => {
   const hour = new Date().getHours();
   let timeName = '';
-  if (hour < 12) return (timeName = 'morning');
-  else if (hour < 18) return (timeName = 'evening');
+  if (hour > 6 && hour < 19) return (timeName = 'morning');
+  else if (hour < 22) return (timeName = 'evening');
   else return (timeName = 'night');
 });
 
@@ -44,12 +44,12 @@ const backgroundImg = {
   '맑음-morning': 'url(src/image/weather/clear_morning.jpg)',
   '맑음-evening': 'url(src/image/weather/clear_afternoon.jpg)',
   '맑음-night': 'url(src/image/weather/clear_night.jpg)',
-  '흐림-morning': 'url(src/image/weather/cloudy.jpg)',
-  '흐림-evening': 'url(src/image/weather/cloudy.jpg)',
-  '흐림-night': 'url(src/image/weather/cloudy_night.jpg)',
   '구름 많음-morning': 'url(src/image/weather/littlecloudy_morning.jpg)',
   '구름 많음-evening': 'url(src/image/weather/littlecloudy.jpg)',
   '구름 많음-night': 'url(src/image/weather/littlecloudy_night.jpg)',
+  '흐림-morning': 'url(src/image/weather/cloudy.jpg)',
+  '흐림-evening': 'url(src/image/weather/cloudy.jpg)',
+  '흐림-night': 'url(src/image/weather/cloudy_night.jpg)',
   '비-morning': 'url(src/image/weather/rain.jpg)',
   '비-evening': 'url(src/image/weather/rain.jpg)',
   '비-night': 'url(src/image/weather/rain.jpg)',
@@ -65,100 +65,100 @@ const weatherBackground = computed(() => {
 </script>
 
 <template>
-  <div class="weather-basetime" v-if="weather">
-    <div class="basetime">날씨예보 기준 시간</div>
-    <div class="basetime">{{ weather.baseTime }}</div>
-  </div>
+  <router-link :to="'/location'" class="button">지역 변경</router-link>
   <div
     class="weather-card"
     v-if="weather"
     :style="{ backgroundImage: weatherBackground }"
   >
-    <div class="weather-location">{{ weather.localName }}</div>
-    <div class="weather-header">
-      <div class="weather-icon">{{ skyEmoji }}</div>
-      <div class="weather-info">
-        <div class="temperature">{{ weather.tem }} ℃</div>
+    <div class="weather-content">
+      <!-- 왼쪽 영역 -->
+      <div class="weather-left">
+        <div class="weather-location">{{ weather.localName }}</div>
         <div class="condition">{{ weather.sky }}</div>
-        <div class="humidity">습도 : {{ weather.reh }}%</div>
       </div>
-      <p class="weather-info">
-        최저/최고 : {{ weather.tmn + '°/' + weather.tmx }}°
-      </p>
+
+      <!-- 오른쪽 영역 -->
+      <div class="weather-right">
+        <div class="warp">
+          <div class="weather-icon">{{ skyEmoji }}</div>
+          <div class="temperature">{{ weather.tem }} ℃</div>
+        </div>
+        <div class="max_min_temperature">
+          최저 : {{ weather.tmn + '° / 최고 : ' + weather.tmx }}°
+        </div>
+        <div class="humidity">습도: {{ weather.reh }}%</div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.weather-live {
-  display: flex;
-  flex-direction: row;
-  max-width: fit-content;
-
-  .weather-livetime {
-    border-radius: 16px 16px 0 0;
-    background: skyblue;
-  }
+.button {
+  margin: 1rem 1rem 0px 1rem;
+  padding: 0 1rem 0.1rem;
+  background-color: #3bbeff;
+  border-radius: 16px 16px 0 0;
 }
 
 .weather-card {
   background-size: cover;
   background-position: center;
   border-radius: 0px 16px 16px 16px;
-  padding: 1.5rem;
-  // max-width: 400px;
-  // box-shadow: 0 4px 16px rgba(0, 0, 0, 0.08);
+  padding: 1rem;
+  margin: 0px 1rem;
+  color: white;
+  max-width: 100%;
+  box-sizing: border-box;
+  text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
 
-  .weather-location {
-    font-size: 2rem;
-    color: white;
-    text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+  .weather-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
+    flex-wrap: wrap;
   }
 
-  .weather-header {
+  .weather-left {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    .weather-location {
+      font-size: 2.4rem;
+      font-weight: bold;
+    }
+
+    .condition {
+      font-size: 1rem;
+    }
+  }
+
+  .warp {
     display: flex;
     align-items: center;
-    gap: 1.2rem;
+    gap: 0.5rem;
+  }
+
+  .weather-right {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 0.3rem;
 
     .weather-icon {
       font-size: 3rem;
-      text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
-      color: #fff;
     }
 
-    .weather-info {
-      display: flex;
-      flex-direction: column;
-      text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.8);
-
-      .temperature {
-        font-size: 2rem;
-        font-weight: 600;
-        color: white;
-      }
-
-      .condition {
-        font-size: 1rem;
-        color: #fff;
-      }
-    }
-  }
-
-  .weather-basetime {
-    margin-top: 1rem;
-    // max-width: 100px;
-    font-size: 0.5rem;
-    color: #222;
-
-    .label {
-      font-weight: 500;
-      color: #000;
+    .temperature {
+      font-size: 2rem;
+      font-weight: bold;
     }
 
-    .basetime {
-      font-size: 0.7rem;
-      color: #222;
-      text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+    .max_min_temperature,
+    .humidity {
+      font-size: 1rem;
     }
   }
 }
