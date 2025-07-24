5f01c44504ea7414abeb5306e7e0f01fc440881c
greenhc502 <greenhc502@gmail.com>
2025-07-24 09:33:38 +0900

    집에서 한거 가져오기

--- src/components/meal/MealAdd.vue
+++ src/components/meal/MealAdd.vue
@@ -9,8 +9,10 @@ const items = reactive({
     foodname:[],
 })
 
-
-
+const getItems = async(searchFood)=>
+{
+    items = await getItems(searchFood);
+}
 const search = () => {
   console.log(searchQuery.foodName);
 };
@@ -18,9 +20,10 @@ const search = () => {
 // 아래는 입력창에 글을 쓰거나 뭘하면 바로바로 받아 들이긴하지만 
 // 시간 텀을 줘서 그 시간 동안 변하는 것에 대한 것을 무시하게 하는 것
  // 1초 후 실행
-const changeText = debounce((value)=>{
+const changeText =  debounce((value)=>{
     const searchFood = value;
     console.log('foodName change:', searchFood);
+    getItems(searchFood);
 }, 1000);
 
 
@@ -31,7 +34,7 @@ const changeText = debounce((value)=>{
   <div class="d-flex flex-row">
     <v-combobox
       v-model="searchQuery.foodName"
-      :items="['양념치킨', '양념후라이드', '후라이드치킨', '족발', '왕족발', 'Wyoming']"
+      :items='items'
       @update:model-value="changeText"
       label="음식명을 입력하세요"
       variant="solo-inverted"
--- src/services/meal/mealService.js
+++ src/services/meal/mealService.js
@@ -1,3 +1,9 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'api/OTD/';
+
+
+
+export const getItems = (item) => {
+    return axios.post('/meal', item).catch((e) => e.response);
+  };
