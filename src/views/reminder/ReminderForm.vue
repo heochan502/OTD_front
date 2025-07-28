9e4b2e8056e6aac5d054a5a4b67405d1dce882a1
d-kjh <dkjh9942@gmail.com>
2025-07-28 21:03:35 +0900

    지미니

--- src/views/reminder/ReminderForm.vue
+++ src/views/reminder/ReminderForm.vue
@@ -242,7 +242,7 @@ const submit = async () => {
         ></textarea>
       </div>
       <button @click="submit">
-        {{ state.reminder.id > 0 ? '수정  하기' : '추가하기' }}
+        {{ state.reminder.id > 0 ? '수정하기' : '추가하기' }}
       </button>
     </div>
   </div>
